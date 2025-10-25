58a3e7f235f20bf103e5e571f3e260086ab593e7
Welton <welton.devv@gmail.com>
2025-10-25 18:07:50 -0300

    bug 404 resolvendo...

---
+++ src/app/login/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+import Header from '../header/page'; // seu componente Header/Login
+
+export default function LoginPage() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
