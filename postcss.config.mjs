2d7cecaabc3588804a7343afd86df36e1f93463e
Welton <welton.devv@gmail.com>
2026-03-15 18:59:42 -0300

    fix: build finalizado e correções de autenticação

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {}, // 👈 O segredo está aqui
+    autoprefixer: {},
   },
 };
 
