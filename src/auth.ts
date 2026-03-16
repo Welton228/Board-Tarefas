47c1ae4c01fe8477660d81a3fcabca8a87b7daef
Welton <welton.devv@gmail.com>
2026-03-15 23:00:38 -0300

    final update mk4

--- src/auth.ts
+++ src/auth.ts
@@ -1,10 +1,35 @@
 import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import prisma from "../src/lib/prisma";
-import { authConfig } from "./auth.config";
+import Google from "next-auth/providers/google";
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  session: { strategy: "jwt" }, // Obrigatório para o Middleware funcionar
-  ...authConfig, // Espalha as configurações do Google e Callbacks
+  providers: [
+    Google({
+      clientId: process.env.AUTH_GOOGLE_ID,
+      clientSecret: process.env.AUTH_GOOGLE_SECRET,
+    }),
+  ],
+  session: {
+    strategy: "jwt",
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (token?.id && session.user) {
+        session.user.id = token.id as string;
+      }
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+  },
+  trustHost: true,
 });
